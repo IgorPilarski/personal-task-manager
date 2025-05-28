51d39fcd9686a7ceac6913df4507a4a61cc05fa0
Igor Pilarski <avgorpl@gmail.com>
2025-05-28 20:55:50 +0200

    develop deleted

--- personal-task-manager/Program.cs
+++ personal-task-manager/Program.cs
@@ -39,11 +39,10 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
